0b6514c96cf4a20bbe602f29ae68ef9540fcc529
patxi <patxiarana05@gmail.com>
2023-09-12 14:21:52 -0300

    configurando estilos globales

--- FestivalMusica_inicio/gulpfile.js
+++ FestivalMusica_inicio/gulpfile.js
@@ -1,14 +1,16 @@
 
 const { src, dest, watch } = require('gulp'); 
 const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
 
-function css (donde) {
+function css (done) {
 src('src/scss/**/*.app.scss') // Identificar el archivo de SASS
+.pipe(plumber())
 .pipe(sass()) //Compilarlo
 .pipe(dest("build/css")); //Guardarlo en el Disco duro 
 
 
-    donde() ; // Callback que avisa a gulp cuando llegamos al final; 
+    done() ; // Callback que avisa a gulp cuando llegamos al final; 
 }
 
 function dev(done){
