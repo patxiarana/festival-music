1af88dbe59034a2f772f5c0f1571763e65b049a6
patxi <patxiarana05@gmail.com>
2023-09-28 11:00:55 -0300

    webp correction function

--- FestivalMusica_inicio/gulpfile.js
+++ FestivalMusica_inicio/gulpfile.js
@@ -1,22 +1,40 @@
 
-const { src, dest, watch } = require('gulp'); 
+const { src, dest, watch, parallel } = require('gulp'); 
+// CSS
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
 
-function css (done) {
-src('src/scss/**/*.app.scss') // Identificar el archivo de SASS
-.pipe(plumber())
-.pipe(sass()) //Compilarlo
-.pipe(dest("build/css")); //Guardarlo en el Disco duro 
+
+//Imagenes 
+const webp = require('gulp-webp')
 
 
-    done() ; // Callback que avisa a gulp cuando llegamos al final; 
+
+
+function css( done ) {
+    src('src/scss/**/*.scss') // Identificar el archivo .SCSS a compilar
+        .pipe( plumber())
+        .pipe( sass() ) // Compilarlo
+        .pipe( dest('build/css') ) // Almacenarla en el disco duro
+    done(); // Callback que avisa a gulp cuando llegamos al final; 
 }
 
-function dev(done){
-    watch("src/scss/**/*.scss", css) ; 
 
+
+function versionWebp(done) {
+    const opciones = {
+        quality: 50
+    }
+
+    src('src/img/**/*.{png,jpg}') // Corrección aquí
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'))
+    done();
+}
+function dev(done){
+    watch('src/scss/**/*.scss', css);
     done()
 }
 exports.css = css ; 
-exports.dev = dev ;
+exports.versionWebp = versionWebp ;
+exports.dev = parallel(dev,versionWebp);
