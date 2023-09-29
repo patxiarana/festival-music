828b3cd7ad0dd6664aee989578c6a1a27160fba0
patxi <patxiarana05@gmail.com>
2023-09-29 11:10:46 -0300

    aligerando imagenes con gulp

--- FestivalMusica_inicio/gulpfile.js
+++ FestivalMusica_inicio/gulpfile.js
@@ -6,6 +6,8 @@ const plumber = require('gulp-plumber');
 
 
 //Imagenes 
+const cache = require('gulp-cache')
+const imagemin = require('gulp-imagemin')
 const webp = require('gulp-webp')
 
 
@@ -19,6 +21,17 @@ function css( done ) {
     done(); // Callback que avisa a gulp cuando llegamos al final; 
 }
 
+function imagenes(done) {
+    const opciones = {
+        optimizationLevel: 3
+    }
+ src('src/img/**/*.{png,jpg}') 
+ .pipe(cache (imagemin(opciones))) 
+ .pipe(dest('build/img')) 
+
+ done() ; 
+}
+
 
 
 function versionWebp(done) {
@@ -26,15 +39,16 @@ function versionWebp(done) {
         quality: 50
     }
 
-    src('src/img/**/*.{png,jpg}') // Corrección aquí
+    src('src/img/**/*.{png,jpg}')
         .pipe(webp(opciones))
         .pipe(dest('build/img'))
     done();
 }
-function dev(done){
+function dev(done){ 
     watch('src/scss/**/*.scss', css);
     done()
 }
 exports.css = css ; 
+exports.imagenes = imagenes ; 
 exports.versionWebp = versionWebp ;
-exports.dev = parallel(dev,versionWebp);
+exports.dev = parallel(imagenes,dev,versionWebp);
