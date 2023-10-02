54ce2c1b146cac9d62b9e710e1a5db5ff07e2de5
patxi <patxiarana05@gmail.com>
2023-10-02 10:40:26 -0300

    primeros pasos con la galeria

---
+++ FestivalMusica_inicio/src/js/app.js
@@ -0,0 +1,15 @@
+
+document.addEventListener('DOMContentLoaded',function(){
+    iniciarApp();
+})
+
+
+function iniciarApp(){
+    crearGaleria();
+}
+
+function crearGaleria(){ 
+    const galeria = document.querySelector('.galeria-imagenes')
+
+    galeria.textContent = 'Vamos a crear la galeria';
+}
