538d57dc121ffc3e7269178f3dfb9f489c98dabf
patxi <patxiarana05@gmail.com>
2023-10-02 11:02:17 -0300

    generando html de la galeria

--- FestivalMusica_inicio/build/js/app.js
+++ FestivalMusica_inicio/build/js/app.js
@@ -11,7 +11,15 @@ function iniciarApp(){
 function crearGaleria(){ 
     const galeria = document.querySelector('.galeria-imagenes')
    
-    for(let i = 0 ; i <= 12 ; i++) {
-        console.log(i)
+    for(let i = 1 ; i <= 12 ; i++) {
+    const imagen = document.createElement('picture');
+
+    imagen.innerHTML=`
+    <source srcset="build/img/thumb/${i}.avif" type="image/avif" >
+    <source srcset="build/img/${i}.webp" type="image/webp" >
+    <img loadign="lazy" width="200" height="300" src="build/img/${i}.jpg" alt="imagen galeria">
+    `; 
+
+    galeria.appendChild(imagen)
     }
 }
